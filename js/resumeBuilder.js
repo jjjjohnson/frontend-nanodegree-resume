8e6ccaf21b6578b351f2db0b315bb434c5ea8ca6
Junjie Jin <jjjjohnsonjin@gmail.com>
2017-07-23 11:26:20 +0800

    Format my name.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,6 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+ var formattedName = HTMLheaderName.replace("%data%", "Junjie Jin");
+// $("#main").append(["Junjie Jin"]);
+ $("#header").append(formattedName);
